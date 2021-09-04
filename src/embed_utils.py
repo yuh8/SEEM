3a14e3b6337e87ed0b501f8a6235d3434665efd1
yuh8 <yhytxwd@gmail.com>
2021-09-04 20:34:41 +1000

    switch to company laptop to verify tensorflow bugs

--- src/embed_utils.py
+++ src/embed_utils.py
@@ -1,27 +1,25 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from tensorflow.keras import layers
-from tensorflow.keras import regularizers
+import tensorflow as tf
 
 
 def conv2d_block(X, num_filters, kernel_size, padding='SAME'):
-    out = layers.Conv2D(filters=num_filters,
-                        kernel_size=kernel_size,
-                        kernel_regularizer=regularizers.L2(1e-4),
-                        strides=1,
-                        padding=padding,
-                        activation=None,
-                        use_bias=False)(X)
-    out = layers.BatchNormalization()(out)
+    out = tf.keras.layers.Conv2D(filters=num_filters,
+                                 kernel_size=kernel_size,
+                                 strides=1,
+                                 padding=padding,
+                                 activation=None,
+                                 use_bias=False)(X)
+    out = tf.keras.layers.BatchNormalization()(out)
     return out
 
 
 def res_block(X, num_filters, kernel_size, padding='SAME'):
     out = conv2d_block(X, num_filters,
                        kernel_size, padding=padding)
-    out = layers.ReLU()(out)
+    out = tf.keras.layers.ReLU()(out)
     out = conv2d_block(X, num_filters,
                        kernel_size, padding=padding)
-    out = layers.Add()([out, X])
-    out = layers.ReLU()(out)
+    out = tf.keras.layers.Add()([out, X])
+    out = tf.keras.layers.ReLU()(out)
     return out
