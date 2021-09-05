ecb90251761ba75ddab33b80a9ef1a608e2842d1
yuh8 <yhytxwd@gmail.com>
2021-09-05 23:40:39 +1000

    fixed a training bug

--- test_model.py
+++ test_model.py
@@ -1,25 +1,22 @@
-import pickle
-import glob
 import numpy as np
+import tensorflow as tf
+from data_gen import data_iterator
 from train_generator import loss_func, get_metrics, get_optimizer, SeedGenerator
 from src.misc_utils import load_json_model
 
 
 if __name__ == "__main__":
     test_path = 'D:/seed_data/generator/test_data/test_batch/'
-    model = load_json_model("generator_model/generator_model.json", SeedGenerator, "SeedGenerator")
-    model.compile(optimizer=get_optimizer(),
-                  loss_fn=loss_func,
-                  metric_fn=get_metrics)
-    model.load_weights("./checkpoints/generator/")
+    with tf.device('/cpu:0'):
+        model = load_json_model("generator_model/generator_model.json", SeedGenerator, "SeedGenerator")
+        model.compile(optimizer=get_optimizer(),
+                      loss_fn=loss_func,
+                      metric_fn=get_metrics)
+        model.load_weights("./checkpoints/generator/")
 
-    for f_name in glob.glob(test_path + 'Xy_*.pkl'):
-        with open(f_name, 'rb') as handle:
-            Xy = pickle.load(handle)
-
-        X_in = Xy[0][0].todense()
-        y = Xy[1].todense()
-        y_pred = model.predict(X_in)
+    for X_in, y in data_iterator(test_path):
+        with tf.device('/cpu:0'):
+            y_pred = model.predict(X_in)
         mask = np.where(X_in[1][0] < 1)
         input = X_in[:10, :10, :-1].sum(-1)
         breakpoint()
