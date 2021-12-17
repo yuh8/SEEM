2ad87a74972c1febcb7d661d339e78ad438f9ccb
yuh8 <yhytxwd@gmail.com>
2021-12-17 18:17:07 +1100

    stable deeper model with layer normalization

--- src/embed_utils.py
+++ src/embed_utils.py
@@ -7,22 +7,20 @@ from tensorflow.keras import regularizers
 def conv2d_block(X, num_filters, kernel_size, padding='SAME'):
     out = tf.keras.layers.Conv2D(filters=num_filters,
                                  kernel_size=kernel_size,
-                                 kernel_regularizer=regularizers.l2(1e-4),
                                  strides=1,
                                  padding=padding,
                                  activation=None,
                                  use_bias=False)(X)
+    out = tf.keras.layers.LayerNormalization(epsilon=1e-9)(out)
     return out
 
 
 def res_block(X, num_filters, kernel_size, padding='SAME'):
     out = conv2d_block(X, num_filters,
                        kernel_size, padding=padding)
-    out = tf.keras.layers.BatchNormalization()(out)
     out = tf.keras.layers.ReLU()(out)
     out = conv2d_block(out, num_filters,
                        kernel_size, padding=padding)
-    out = tf.keras.layers.BatchNormalization()(out)
     out = tf.keras.layers.Add()([out, X])
     out = tf.keras.layers.ReLU()(out)
     return out
